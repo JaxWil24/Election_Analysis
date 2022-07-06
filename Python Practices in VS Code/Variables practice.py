9b085f55bdb3cb03f2bc2f5af5c4ae03a240cc06
Jacqueline Wilson <jacqueline.carver.24@gmail.com>
2022-07-06 14:12:18 -0400

    Adding new PyPoll.py files, election_analysis.txt file, and election_results.csv files

---
+++ Python Practices in VS Code/Variables practice.py
@@ -0,0 +1,20 @@
+# Creates a variable named with a string "Frankfurter"
+title = "Frankfurter"
+
+# Creates a variable with an integer 80
+years = 80
+
+# Creates a variable with the boolean value of True
+expert_status = True
+
+# Prints a statement adding the variable
+print("Nick is a professional" + title)
+
+# Convert the integer years into a string and prints
+print("He has been coding for " + str(years))
+
+# Converts a boolean into a string and prints
+print("Expert Status: " + str(expert_status))
+
+# An f-string accepts all data types without conversion
+print(f"Expert Status: {expert_status})
